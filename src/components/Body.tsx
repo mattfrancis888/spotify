5bd4898e44b772c0d60ddaac92b6bdb5c723d041
Matthew Francis <mattfrancis888@gmail.com>
2020-09-06 21:27:49 -0400

    <Body/> has a responsive layout for all screens.

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,10 +1,38 @@
 import React from "react";
-
+import edSheeran from "../img/edSheeran.jpg";
+import spotify from "../img/spotify.png";
 const Body: React.FC<{}> = () => {
     return (
         <React.Fragment>
-            <div className="homeContianer">
-                <div className="homeBlackGradient"></div>
+            <div className="homeContainer">
+                <div className="titleWrap">
+                    <img
+                        className="spotifyLogo"
+                        src={spotify}
+                        alt="spotify logo"
+                    ></img>
+                    <h1 className="favoriteArtistTitle">'s Favorite Artists</h1>
+                </div>
+                <div className="favoriteArtistsContainer">
+                    <div className="artistCircle">
+                        <img src={edSheeran} alt="artist" />
+                    </div>
+                    <div className="artistCircle">
+                        <img src={edSheeran} alt="artist" />
+                    </div>
+                    <div className="artistCircle">
+                        <img src={edSheeran} alt="artist" />
+                    </div>
+                    <div className="artistCircle">
+                        <img src={edSheeran} alt="artist" />
+                    </div>
+                    <div className="artistCircle">
+                        <img src={edSheeran} alt="artist" />
+                    </div>
+                    <div className="artistCircle">
+                        <img src={edSheeran} alt="artist" />
+                    </div>
+                </div>
             </div>
         </React.Fragment>
     );
